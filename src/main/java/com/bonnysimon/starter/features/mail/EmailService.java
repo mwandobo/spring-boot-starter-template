50716e8f78f7608d201011a5befaa82f8bc4ee56
boniface mwandobo <bonifacesimon04@gmail.com>
2025-11-20 16:45:11 +0300

    refactor introduction to send notifications

--- src/main/java/com/bonnysimon/starter/features/mail/EmailService.java
+++ src/main/java/com/bonnysimon/starter/features/mail/EmailService.java
@@ -7,12 +7,17 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
 
     public void sendSimpleEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -24,14 +29,21 @@ public class EmailService {
     }
 
     public void sendHtmlEmail(          EmailPayload emailPayload) throws MessagingException {
-//        public void sendHtmlEmail(          String to, String subject, String htmlContent) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
         String[] recipients = emailPayload.getTo().toArray(new String[0]);
 
+        Context thymeleafContext = new Context();
+        thymeleafContext.setVariables(emailPayload.getContext());
+
+        String htmlContent = templateEngine.process(
+                "email/" + emailPayload.getTemplate(),
+                thymeleafContext
+        );
+
         helper.setTo(recipients);
         helper.setSubject(emailPayload.getSubject());
-        helper.setText(emailPayload.getTemplate(), true);
+        helper.setText(htmlContent, true);
         helper.setFrom("Mwalimu Commercial Bank <no-reply@mwalimucommercialbank.co.tz>");
         mailSender.send(mimeMessage);
     }
